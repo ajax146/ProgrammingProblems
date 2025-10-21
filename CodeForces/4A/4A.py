87169bc28943494fa05ec005af8aa8483a4a9eed
ajax146 <31014239+ajax146@users.noreply.github.com>
2025-10-21 18:53:08 -0400

    Problem 4A

---
+++ CodeForces/4A/4A.py
@@ -0,0 +1,8 @@
+# Read input
+w = int(input())
+
+# Be greedy with the case, 2 cannot be divided into 2 non-zero positive integers, so we have to avoid it.
+if w <= 3:
+    print("NO")
+else:
+    print("YES" if w % 2 == 0 else "NO")

ab1a40cd8f664baa287795b0c5842318930c79b9
ajax146 <31014239+ajax146@users.noreply.github.com>
2025-10-22 15:19:51 -0400

    1807A in swift

---
+++ CodeForces/1807A/1807A.swift
@@ -0,0 +1,12 @@
+import Foundation
+
+let w = Int(readLine() ?? "") ?? 0
+
+for _ in 0...(w-1) {
+    let nums = readLine()!.split(separator: " ").map { Int($0)! }
+    if nums[0] + nums[1] == nums[2] {
+        print("+")
+    } else {
+        print("-")
+    }
+}

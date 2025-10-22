90e3fbcffd2bb35c38f6d3c4cac59ea85f18f5ea
ajax146 <31014239+ajax146@users.noreply.github.com>
2025-10-22 15:25:09 -0400

    Update swift code

--- CodeForces/1807A/1807A.swift
+++ CodeForces/1807A/1807A.swift
@@ -1,9 +1,9 @@
 import Foundation
 
-let w = Int(readLine() ?? "") ?? 0
+var w = Int(readLine() ?? "") ?? 0
 
 for _ in 0...(w-1) {
-    let nums = readLine()!.split(separator: " ").map { Int($0)! }
+    var nums = readLine()!.split(separator: " ").map { Int($0)! }
     if nums[0] + nums[1] == nums[2] {
         print("+")
     } else {

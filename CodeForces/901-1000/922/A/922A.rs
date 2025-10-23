86e8fc1d0ca3ce0d2ba54ea2a0c4a7070cb322b8
ajax146 <31014239+ajax146@users.noreply.github.com>
2025-10-23 11:51:00 -0400

    922A in rust

---
+++ CodeForces/901-1000/922/A/922A.rs
@@ -0,0 +1,46 @@
+use std::io;
+
+fn main() {
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).unwrap();
+    let nums: Vec<i64> = input
+        .split_whitespace()
+        .map(|x| x.parse::<i64>().unwrap())
+        .collect();
+
+    let clone = nums[0];
+    let original = nums[1];
+
+
+
+    // We can never make more originals than clones
+    if (original-1) > clone {
+        println!("NO");
+        return;
+    }
+
+    let originals_to_make = original-1;
+    // In any case we have to make negative originals, stop
+    if originals_to_make < 0 {
+        println!("NO");
+        return;
+    }
+
+    // If we are supposed to make a clone, but cannot make any more originals, we cannot make only clones
+    if originals_to_make == 0 && clone != 0 {
+        println!("NO");
+        return;
+    }
+
+    // Every original makes a clone.
+    let clones_to_make = clone-originals_to_make;
+    // If we have to make negative clones, stop.
+    if clones_to_make < 0 {
+        println!("NO");
+    } else if clones_to_make % 2 == 0 {
+        println!("YES");
+    } else {
+        println!("NO");
+    }
+
+}

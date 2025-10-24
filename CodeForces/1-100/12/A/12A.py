d593f7563b6219231672093d9779b649fc0d1057
ajax146 <31014239+ajax146@users.noreply.github.com>
2025-10-24 17:05:12 -0400

    12A in Python

---
+++ CodeForces/1-100/12/A/12A.py
@@ -0,0 +1,6 @@
+input_str = input() + input() + input()
+
+if input_str[0] == input_str[8] and input_str[1] == input_str[7] and input_str[2] == input_str[6] and input_str[3] == input_str[5]:
+    print("YES")
+else:
+    print("NO")

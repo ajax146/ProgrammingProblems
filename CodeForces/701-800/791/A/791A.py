44ba1facf58d8ce1c1702f2f3873c19c76f819d6
ajax146 <31014239+ajax146@users.noreply.github.com>
2025-10-27 08:09:31 -0700

    Add missing comment 791A.py

--- CodeForces/701-800/791/A/791A.py
+++ CodeForces/701-800/791/A/791A.py
@@ -1,3 +1,10 @@
+# Problem 791A: Bear and Big Brother
+# https://codeforces.com/contest/791/problem/A
+# Executed with: Python 3.11.2
+# Linux version: Linux kernel 6.1.0-10-amd64
+# Submitted on: October 23rd, 2025
+# Codeforces language used: PyPy 3.10 (7.3.15, 64bit)
+
 limak, bob = map(int, input().split())
 year = 1
 while True:
